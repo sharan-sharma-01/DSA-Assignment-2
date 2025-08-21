b9c2580890aa98f241f9d3f7a3614c9580e397bc
Sharan Sharma <sharan22042006@gmail.com>
2025-08-21 09:34:02 +0530

    q3b.cpp

---
+++ q3b.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int findMissingBinary(int arr[], int n) {
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == mid + 1) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return low + 1;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int missing = findMissingBinary(arr, n);
+    cout << "Missing number (Binary Search): " << missing << endl;
+
+    return 0;
+}
